c5f394bdd83143e473cbbfa341954901380442a3
Edik Huloian <guloyan.edik@gmail.com>
2024-01-15 22:12:56 -0800

    sumarry

---
+++ Project_app/construction/component.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+      <h1>Hello, Vue with TypeScript and Vue Router!</h1>
+    </div>
+  </template>
+  
+  <script lang="ts">
+  import { defineComponent } from 'vue';
+  
+  export default defineComponent({
+    name: 'Home',
+  });
+  </script>
+  
+  <style scoped>
+  /* Ваши стили здесь */
+  </style>
+  
